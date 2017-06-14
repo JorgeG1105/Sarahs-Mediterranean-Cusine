99bc00eceab4b8ba95de42ef891d47651eb6ba42
Jorge Goris <J_goris@live.com>
2017-06-13 22:51:33 -0400

    Version 2.0
    Got the slide working thanks to Slick but need to shrink it and make it
    mobile responsive.

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slideShow').slick({
+    arrows: false,
+    dots: false,
+    fade: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+});
